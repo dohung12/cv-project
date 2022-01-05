3f72c92b902e1c51eaa4f1cd6da8ae40bc40dbde
dohung <dohuyhung12@gmail.com>
2022-01-05 20:47:06 +0700

    install uniqid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import GeneralInformation from "./components/GeneralInformation";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="App">
       <GeneralInformation />
+      <Experience />
+      <Education />
     </div>
   );
 }
