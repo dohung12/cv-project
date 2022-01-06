2a8b722d9ce5bf28b22c2a984c5261a9dc8e0254
dohung <dohuyhung12@gmail.com>
2022-01-06 13:37:26 +0700

    hide label

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -3,7 +3,7 @@ import "./InputField.css";
 const InputField = ({ name, label, type, value, placeholder, onChange }) => {
   return (
     <div className={name}>
-      <label htmlFor={name}>{label}: </label>
+      {/* <label htmlFor={name}>{label}: </label> */}
       <input
         type={type}
         name={name}
